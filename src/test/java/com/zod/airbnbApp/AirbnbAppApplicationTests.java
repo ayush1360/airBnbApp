725733384d0ce7ba2cda99e5b15b1385e88af11b
ayushs13600 <ayushs13600@gmail.com>
2026-06-15 10:54:50 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/zod/airbnbApp/AirbnbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zod.airbnbApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
